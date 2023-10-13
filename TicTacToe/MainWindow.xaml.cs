b0cc04e0540ca065b4658ffbec731deaf3718360
Aizhan <aizhankarasartova@gmail.com>
2023-10-14 01:32:13 +0600

    constructor and variables

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TicTacToe
 {
@@ -20,9 +8,41 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+        /// <summary>
+        /// Holds the current results of cells in the active game
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if it is player 1's turn (X) or player 2's turn (O)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        /// True if the game has ended
+        /// </summary>
+        private bool mGameEnded;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            
         }
     }
 }
---
+++ TicTacToe/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToe
+{
+    /// <summary>
+    /// The type of value of cell in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is 0
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is x
+        /// </summary>
+        Cross
+    }
+}
